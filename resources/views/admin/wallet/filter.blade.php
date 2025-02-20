47daf566c7980956efa90e9076d4f11c36d4b5c7
Krishna Mishra <er.krishna.mishra@gmail.com>
2025-02-20 15:20:17 +0530

    dd

--- resources/views/admin/wallet/filter.blade.php
+++ resources/views/admin/wallet/filter.blade.php
@@ -2,7 +2,7 @@
     <div class="col-12">
         <div class="card">
             <div class="card-body">
-                <form action="{{ route('admin.transactions.index') }}" id="filterForm">
+                <form action="{{ route('admin.wallet.index') }}" id="filterForm">
                      <div class="row">
                         <div class="col-sm-12 mb-1 d-flex align-items-center">
                             <label class="col-form-label me-2" for="franchise">Franchises:</label>
@@ -19,7 +19,7 @@
                             <button type="submit" class="btn btn-sm btn-danger me-1" form="filterForm">
                                 <i class="mdi mdi-filter"></i> Filter
                             </button>
-                            <a href="{{ route('admin.transactions.index') }}" class="btn btn-sm btn-primary">
+                            <a href="{{ route('admin.wallet.index') }}" class="btn btn-sm btn-primary">
                                 <i class="mdi mdi-refresh"></i> Reset
                             </a>
                         </div>
