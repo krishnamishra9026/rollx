e036ab84715dc9c537eff79838fe9e4d1c0363cd
Krishna Mishra <er.krishna.mishra@gmail.com>
2025-02-21 00:36:52 +0530

    chnages

--- resources/views/franchise/orders/sales/reports/filter.blade.php
+++ resources/views/franchise/orders/sales/reports/filter.blade.php
@@ -5,26 +5,12 @@
                 <form action="{{ route('franchise.sale.reports.index') }}" id="filterForm">
                     <div class="row">
 
-                        <div class="col-sm-3 mb-2">
+                        <div class="col-sm-4 mb-2">
                             <label class="col-form-label" for="order_date">Order Date</label>
                             <input type="date" class="form-control" id="date" name="order_date" placeholder="Enter Start date" value="{{ request('date') }}">
                         </div>
 
-                        
-
-                        <div class="col-sm-3 mb-2">
-                            <label class="col-form-label" for="franchise">Franchises</label>
-                            <select class="form-select" id="franchise" name="franchise">
-                                <option value="">All</option>
-                                @foreach($franchises as $franchise)
-                                <option value="{{ $franchise->id }}" {{ request('franchise') == $franchise->id ? 'selected' : '' }}>
-                                    {{ $franchise->firstname }} {{ $franchise->lastname }}
-                                </option>
-                                @endforeach
-                            </select>
-                        </div>
-
-                        <div class="col-sm-3 mb-2">
+                        <div class="col-sm-4 mb-2">
                             <label class="col-form-label" for="product">Products</label>
                             <select class="form-select" id="product" name="product">
                                 <option value="">All</option>
@@ -38,7 +24,7 @@
 
                        
 
-                        <div class="col-sm-3 mb-2">
+                        <div class="col-sm-4 mb-2">
                             <label class="col-form-label" for="statuses">Status</label>
                             <select class="form-select" id="statuses" name="status">
                                 <option value="">All</option>
