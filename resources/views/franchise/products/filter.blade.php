e724b8f3fcf7eb91be7a2cbb1bc9ae21e9c9ef79
Krishna Mishra <er.krishna.mishra@gmail.com>
2025-02-21 18:33:52 +0530

    ddjd

--- resources/views/franchise/products/filter.blade.php
+++ resources/views/franchise/products/filter.blade.php
@@ -5,17 +5,27 @@
                 <form action="{{ route('franchise.products.index') }}" id="filterForm">
                     <div class="row">
                         <div class="col-sm-8">
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter Product name" value="{{ $filter['name'] }}">
-                        
 
-                        <a href="{{ route('franchise.products.index') }}" class="btn btn-sm btn-primary float-end me-1"><i
+                        <div class="col-sm-6">
+
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter Product name" value="{{ $filter['name'] }}">
+
+                        </div>
+
+
+                        <div class="col-sm-6">
+
+                        <a href="{{ route('franchise.products.index') }}" class="btn btn-sm btn-primary me-1"><i
                             class="mdi mdi-refresh"></i> Reset</a>
                             
-                        <button type="submit" class="btn btn-sm btn-danger float-end me-1" form="filterForm"><i
+                        <button type="submit" class="btn btn-sm btn-danger me-1" form="filterForm"><i
                                 class="mdi mdi-filter"></i> Filter</button>
 
-                        <button type="submit" class="btn btn-sm btn-success float-end me-1" form="CreateOrders">Create Orders</button>
+
+
+                        <button type="submit" class="btn btn-sm btn-success me-1" form="CreateOrders">Create Orders</button>
                         </div>
+                    </div>
 
                         <div class="col-sm-4 mt-2 text-end">
                             Points : <span class="points">{{ auth()->user()->balance }}</span>
