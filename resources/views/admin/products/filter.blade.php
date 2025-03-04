5531e0da82f7b1c7422f4290e841b9fb744fafdf
Krishna Mishra <er.krishna.mishra@gmail.com>
2025-03-04 17:47:25 +0530

    chnage

--- resources/views/admin/products/filter.blade.php
+++ resources/views/admin/products/filter.blade.php
@@ -9,14 +9,6 @@
                             <label class="col-form-label" for="name">Product Name</label>
                             <input type="text" class="form-control" id="name" name="name" placeholder="Enter product name" value="{{ $filter['name'] }}">
                         </div>
-                        <div class="col-sm-4 mb-1">
-                            <label class="col-form-label" for="model_number">Model Number</label>
-                            <input type="text" class="form-control" id="model_number" name="model_number" placeholder="Enter model number" value="{{ $filter['model_number'] }}">
-                        </div>
-                        <div class="col-sm-4 mb-1">
-                            <label class="col-form-label" for="serial_number">Serial Number</label>
-                            <input type="text" class="form-control" id="serial_number" name="serial_number" placeholder="Enter serial number" value="{{ $filter['serial_number'] }}">
-                        </div>
                         <div class="col-sm-12 mt-2">
                             <div class="d-flex flex-wrap justify-content-end gap-1">
                                 <a href="{{ route('admin.products.create') }}" class="btn btn-sm btn-dark">
