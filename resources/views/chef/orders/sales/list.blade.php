3172c61df27dfe915f6860c7f8192c718f26ebb0
Krishna Mishra <er.krishna.mishra@gmail.com>
2025-03-05 18:24:29 +0530

    dd

--- resources/views/chef/orders/sales/list.blade.php
+++ resources/views/chef/orders/sales/list.blade.php
@@ -99,8 +99,8 @@
 
             <div class="col-xl-3 col-lg-4">
                 <div class="card tilebox-one">
-                    <div class="card-body text-center">                    
-                        <h5 class="mt-0">Total Quantity</h5>
+                    <div class="card-body text-center btn btn-primary">                    
+                        <h5 class="mt-0 text-uppercase">Total Quantity</h5>
                         <h2 class="my-2" id="active-users-count">{{ $sales[0]->order->quantity }}</h2>
                         <a class="mb-0 text-dark" href="{{ route('chef.orders.index') }}">    
                             <small>View Details </small>                   
@@ -111,8 +111,8 @@
 
             <div class="col-xl-3 col-lg-4">
                 <div class="card tilebox-one">
-                    <div class="card-body text-center">                    
-                        <h5 class="mt-0">Sold Quantity</h5>
+                    <div class="card-body text-center btn btn-secondary">                    
+                        <h5 class="mt-0 text-uppercase">Sold Quantity</h5>
                         <h2 class="my-2" id="active-users-count">{{ $sales[0]->order->quantity  - $sales[0]->order->stock }}</h2>
                         <a class="mb-0 text-dark" href="{{ route('chef.orders.index') }}">    
                             <small>View Details </small>                   
@@ -123,8 +123,8 @@
 
             <div class="col-xl-3 col-lg-4">
                 <div class="card tilebox-one">
-                    <div class="card-body text-center">                    
-                        <h5 class="mt-0">Remaining Quantity</h5>
+                    <div class="card-body text-center btn btn-success">                    
+                        <h5 class="mt-0 text-uppercase">Remaining Quantity</h5>
                         <h2 class="my-2" id="active-users-count">{{ $sales[0]->order->stock }}</h2>
                         <a class="mb-0 text-dark" href="{{ route('chef.orders.index') }}">    
                             <small>View Details </small>                   
@@ -135,8 +135,8 @@
 
              <div class="col-xl-3 col-lg-4">
                 <div class="card tilebox-one">
-                    <div class="card-body text-center">                    
-                        <h5 class="mt-0">Not Sold Quantity</h5>
+                    <div class="card-body text-center btn btn-danger">                    
+                        <h5 class="mt-0 text-uppercase">Not Sold Quantity</h5>
                         <h2 class="my-2" id="active-users-count">{{ $sales[0]->order->quantity  - $sales[0]->order->stock }}</h2>
                         <a class="mb-0 text-dark" href="{{ route('chef.orders.index') }}">    
                             <small>View Details </small>                   
