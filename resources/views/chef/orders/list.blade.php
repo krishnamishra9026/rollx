f478bb373e7ea2373dc6e1825cc163e7f0f1c0d5
Krishna Mishra <er.krishna.mishra@gmail.com>
2025-03-17 19:40:47 +0530

    d

--- resources/views/chef/orders/list.blade.php
+++ resources/views/chef/orders/list.blade.php
@@ -41,6 +41,7 @@
                                             <th class="fw-bold">Sub Total</th>
                                             <th class="fw-bold">Total</th>
                                             <th class="fw-bold">Stock</th>
+                                            <th class="fw-bold">Sales</th>
                                             <th class="fw-bold">Order Date Time</th>
                                             <th class="fw-bold">Action</th>
                                         </tr>
@@ -54,6 +55,7 @@
                                                 <td>{{ $order->sub_total }}</td>
                                                 <td>{{ $order->total }}</td>
                                                 <td>{{ $order->stock }}</td>
+                                                <td>{{ $order->sales->where('chef_id', auth()->user()->id)->count() }}</td>
                                                 <td>{{ \Carbon\Carbon::parse($order->created_at)->format('d-m-Y H:i A') }}</td>
                                         
                                                 <td>
