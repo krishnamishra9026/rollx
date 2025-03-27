90078eba01744b9be1401976a743084b6d88d3a8
Krishna Mishra <er.krishna.mishra@gmail.com>
2025-03-27 16:29:31 +0530

    chnages

--- app/Imports/Admin/ProductsImport.php
+++ app/Imports/Admin/ProductsImport.php
@@ -21,9 +21,14 @@ class ProductsImport implements ToModel, WithHeadingRow
 
             return new Product([
                 'name' => $row['name'],
-                'outlet_name' => $row['outlet_name'],
+                'outlet_name' => $row['outlet_name'] ?? '',
                 'quantity' => $row['quantity'],
+                'sold_quantity' => 0,
+                'available_quantity' => $row['quantity'],
                 'price' => $row['price'],
+                'sold_color' => $row['sold_color'] ?? '',
+                'selling_type' => $row['selling_type'] ?? '',
+                'status' => $row['status'] ?? 0,
             ]);
         }
 
